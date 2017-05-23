87a12495c13fb8c02eabdeeb4f4d8adb3fb93def
Runnan Zhong <r.tina.zhong@gmail.com>
2017-05-23 18:00:53 -0400

    all tests passing except button feature to display availability for inventory

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,28 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def create
+    @product = Product.create(name: params[:name], price: params[:price], description: params[:description], inventory: params[:inventory])
+    redirect_to products_path
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    if product.inventory > 0
+      render plain: "true"
+    else
+      render plain: "false"
+    end
+
+
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+  # resources :products
+  get '/products', to: 'products#index'
+  get '/products/:id/description', to: 'products#description'
+  post '/products', to: 'products#create'
+  get '/products/:id/inventory', to: 'products#inventory'
+  # get '/'
+
+
 end
